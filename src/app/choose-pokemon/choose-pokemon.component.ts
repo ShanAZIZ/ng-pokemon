a6938ffa26cd563a8261e861d8aa9cd663ad8f80
Shan AZIZ <shan.azizh@gmail.com>
2022-06-23 09:36:07 +0200

    feat(pokemon): choose pokemon

--- src/app/choose-pokemon/choose-pokemon.component.ts
+++ src/app/choose-pokemon/choose-pokemon.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Pokemon, PokemonName } from '../pokemon/pokemon.component';
+import { Component, OnInit } from '@angular/core';
+import { PokemonName } from '../pokemon/pokemon.component';
 import { PokemonService } from '../pokemon.service';
 
 @Component({
@@ -8,18 +8,48 @@ import { PokemonService } from '../pokemon.service';
   styleUrls: ['./choose-pokemon.component.css']
 })
 export class ChoosePokemonComponent implements OnInit {
-  @Input()
-  pokemonNames: PokemonName[] = [];
-  @Output() next: EventEmitter<any> = new EventEmitter();
-  @Output() previous: EventEmitter<any> = new EventEmitter();
-
-  pokemons: Pokemon[] = [];
+  pokemons: PokemonName[] = [];
+  offset: number = 0;
+  selectedPokemon: string = '';
 
   constructor(private pokemonService: PokemonService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.setPokemonNames();
+  }
 
-  setPokemon(pokemonName: PokemonName) {}
+  setPokemonNames(): void {
+    this.pokemonService.fetchPokemonNames(this.offset).subscribe({
+      next: (res) => {
+        if (res['results']) {
+          console.log(res);
+          this.pokemons = res['results'];
+          this.pokemons = res['results'];
+        }
+      },
+      error: (err) => {
+        console.log(err);
+        // TODO: Logger Service
+      }
+    });
+  }
+
+  next() {
+    this.offset += 9;
+    this.setPokemonNames();
+  }
+
+  previous() {
+    if (this.offset > 8) {
+      this.offset -= 8;
+      this.setPokemonNames();
+    }
+  }
+
+  selectPokemon(name: string) {
+    this.selectedPokemon = name;
+    console.log('SELECTED', name);
+  }
 }
 
 //  `http://pokemon.lux.frachet.berthelot.io/api/img/${name}.gif`
--- src/app/pokemon-menu/pokemon-menu.component.ts
+++ src/app/pokemon-menu/pokemon-menu.component.ts
@@ -2,46 +2,10 @@
 
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../pokemon.service';
-import { PokemonName } from '../pokemon/pokemon.component';
 
 @Component({
   selector: 'app-pokemon-menu',
   templateUrl: './pokemon-menu.component.html',
   styleUrls: ['./pokemon-menu.component.css']
 })
-export class PokemonMenuComponent implements OnInit {
-  pokemonNames: PokemonName[] = [];
-  offset: number = 0;
-
-  constructor(private pokemonService: PokemonService) {}
-
-  ngOnInit(): void {
-    this.setPokemonNames();
-  }
-
-  setPokemonNames(): void {
-    this.pokemonService.fetchPokemonNames(this.offset).subscribe({
-      next: (res) => {
-        if (res['results']) {
-          this.pokemonNames = res['results'];
-        }
-      },
-      error: (err) => {
-        console.log(err);
-        // TODO: Logger Service
-      }
-    });
-  }
-
-  next() {
-    this.offset += 9;
-    this.setPokemonNames();
-  }
-
-  previous() {
-    if (this.offset > 8) {
-      this.offset -= 8;
-      this.setPokemonNames();
-    }
-  }
-}
+export class PokemonMenuComponent implements OnInit {}
