ce01978586e0cd60956330d1ef69fb33ae75cde4
Marwan <marwan.boubchir@outlook.fr>
2022-06-23 11:07:26 +0200

    feature(html/css): battle affichage des pokemons.

--- src/app/battle/battle.component.ts
+++ src/app/battle/battle.component.ts
@@ -7,11 +7,14 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./battle.component.css']
 })
 export class BattleComponent implements OnInit {
+  pokemon1: string = '';
+  pokemon2: string = '';
   constructor(private route: ActivatedRoute) {}
-
   ngOnInit(): void {
     this.route.queryParams.subscribe((params: Params): void => {
       console.log(params);
+      this.pokemon1 = params['pokemon1'];
+      this.pokemon2 = params['pokemon2'];
     });
   }
 }
