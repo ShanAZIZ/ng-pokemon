2d8dcf028dc63d214d575a14a6143775b8c60110
Shan AZIZ <shan.azizh@gmail.com>
2022-06-23 09:37:38 +0200

    feat(pokemon): fix

--- src/app/pokemon-menu/pokemon-menu.component.ts
+++ src/app/pokemon-menu/pokemon-menu.component.ts
@@ -1,11 +1,12 @@
 // @ts-ignore
 
 import { Component, OnInit } from '@angular/core';
-import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-pokemon-menu',
   templateUrl: './pokemon-menu.component.html',
   styleUrls: ['./pokemon-menu.component.css']
 })
-export class PokemonMenuComponent implements OnInit {}
+export class PokemonMenuComponent implements OnInit {
+  ngOnInit() {}
+}
